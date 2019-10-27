66265f034ea35f37ca76de5fb17e82c961ed48c5
juju <gliheng@gmail.com>
2019-10-27 19:57:59 +0800

    fix deps

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ import setuptools
 with open('README.md', 'r') as file:
     long_description = file.read()
 
+with open('requirements.txt') as file:
+    required = file.read().splitlines()
+
 setuptools.setup(
     name='psi-cli',
-    version='0.0.1',
+    version='0.0.2',
     author='juju',
     author_email='gliheng@live.com',
     description='flutter-rs devtool',
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    install_requires=required,
     url='https://github.com/flutter-rs/psi-cli',
     packages=setuptools.find_packages(),
     entry_points ={ 
